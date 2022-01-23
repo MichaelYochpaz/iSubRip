3c7454613379c3dc4ecd16eab3e0dc12e27dfe4e
Michael <8832013+MichaelYochpaz@users.noreply.github.com>
2022-01-23 21:20:35 +0200

    Move constants to a separate file

--- isubrip/utils.py
+++ isubrip/utils.py
@@ -2,23 +2,12 @@ import sys
 import os
 import tomli
 from typing import Union, Any
-from xdg import xdg_config_home
 from mergedeep import merge
 
+from isubrip.constants import *
 from isubrip.types import SubtitlesType
 from isubrip.exceptions import DefaultConfigNotFound, UserConfigNotFound
 
-DEFAULT_CONFIG_PATH = "resources/default_config.toml"
-USER_CONFIG_PATH_WINDOWS = f"{os.getenv('appdata')}\\iSubRip\\config.toml"
-USER_CONFIG_PATH_LINUX = f"{xdg_config_home().resolve()}/iSubRip/config.toml"
-USER_CONFIG_PATH_MACOS = r"~/Library/Application Support/isubrip/config.toml"
-
-VALID_ARCHIVE_FORMATS = ["zip", "tar", "gztar"]
-
-ITUNES_STORE_REGEX = r"https?://itunes.apple.com/[a-z]{2}/movie/[a-zA-Z0-9\-%]+/id[0-9]+($|(\?.*))"
-
-
-# -------------------- Functions ------------------- #
 
 def parse_config(user_config_path: Union[str, None] = None) -> dict[str, Any]:
     """Parse and config file and save settings to a dictionary.
