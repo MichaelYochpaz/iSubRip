3230c01e4feca29766d3da98a77b09861575022b
Michael <8832013+MichaelYochpaz@users.noreply.github.com>
2022-01-24 11:38:41 +0200

    Change subtitles type in file name to lower case

--- isubrip/__main__.py
+++ isubrip/__main__.py
@@ -176,7 +176,7 @@ def format_file_name(title: str, language_code: str, subtitles_type: SubtitlesTy
     file_name = f"{format_title(title)}.iT.WEB.{language_code}"
 
     if subtitles_type is not SubtitlesType.NORMAL:
-        file_name += '.' + subtitles_type.name
+        file_name += '.' + subtitles_type.name.lower()
 
     return file_name
 
