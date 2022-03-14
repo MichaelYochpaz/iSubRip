fc7e504f059d414be0140f292af9eb35015c829d
Michael <8832013+MichaelYochpaz@users.noreply.github.com>
2022-03-14 22:20:49 +0200

    Fix a bad config value reference

--- isubrip/__main__.py
+++ isubrip/__main__.py
@@ -124,7 +124,7 @@ def main() -> None:
                 shutil.rmtree(current_download_path)
                 atexit.unregister(shutil.rmtree)
 
-            print(f"\n{len(downloaded_subtitles)} matching subtitles for \"{movie_data.name}\" were found and downloaded to \"{os.path.abspath(config['downloads']['folder'])}\".")
+            print(f"\n{len(downloaded_subtitles)} matching subtitles for \"{movie_data.name}\" were found and downloaded to \"{os.path.abspath(config.downloads['folder'])}\".")
 
 
 def find_appdata_path() -> str:
